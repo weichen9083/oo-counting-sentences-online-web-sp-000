0777dc79498eb1ca6ad438e870ef557a2407e971
wei chen <weichen9083@gmail.com>
2019-10-01 21:56:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") 
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    
+  self.end_with?("!")
 
   end
 
   def count_sentences
+
+count_array = self.split(/[.!?]/)
+
+new_array = count_array.reject {|what| what.empty?}
+
+
+if new_array.length == 0
+  return 0
+else 
+  return new_array.length
+end 
 
   end
 end
