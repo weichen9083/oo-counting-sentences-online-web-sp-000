18e5ced6be0b929ec7ab407842094dda80a62a50
wei chen <weichen9083@gmail.com>
2020-12-01 22:18:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -5,6 +5,14 @@ class String
   def sentence?
     self.end_with?(".")
   end 
+  
+  def question?
+    self.end_with?("?")
+  end 
+  
+  def exclamation?
+    self.end_with?("!")
+  end 
 
   
 end
